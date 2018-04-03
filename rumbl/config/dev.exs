e42b27ddc1ffb209fd783850297b629c02699257
Rafael Roman <31549567+RomanNumerals@users.noreply.github.com>
2018-04-02 22:08:51 -0400

    commented out postgres username/password which caused fatal error upon running local server

--- rumbl/config/dev.exs
+++ rumbl/config/dev.exs
@@ -11,9 +11,9 @@ config :rumbl, Rumbl.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin",
-                    cd: Path.expand("../", __DIR__)]]
-
+  watchers: [
+    node: ["node_modules/brunch/bin/brunch", "watch", "--stdin", cd: Path.expand("../", __DIR__)]
+  ]
 
 # Watch static and templates for browser reloading.
 config :rumbl, Rumbl.Endpoint,
@@ -36,8 +36,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :rumbl, Rumbl.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  # username: "postgres",
+  # password: "postgres",
   database: "rumbl_dev",
   hostname: "localhost",
   pool_size: 10
