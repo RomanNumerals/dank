1d4ce62e408e0f3803be6007b3a1e8b5cf7126df
Rafael Roman <31549567+RomanNumerals@users.noreply.github.com>
2018-04-13 13:53:32 -0400

    setup necessary plugins and code change for the ecto queries/constraints, from chapter 7

--- rumbl/web/models/category.ex
+++ rumbl/web/models/category.ex
@@ -2,7 +2,7 @@ defmodule Rumbl.Category do
   use Rumbl.Web, :model
 
   schema "categories" do
-    field :name, :string
+    field(:name, :string)
 
     timestamps()
   end
@@ -14,5 +14,13 @@ defmodule Rumbl.Category do
     struct
     |> cast(params, [:name])
     |> validate_required([:name])
+  end
+
+  def alphabetical(query) do
+    from(c in query, order_by: c.name)
+  end
+
+  def names_and_ids(query) do
+    from(c in query, select: {c.name, c.id})
   end
 end
--- rumbl/web/models/video.ex
+++ rumbl/web/models/video.ex
@@ -6,6 +6,7 @@ defmodule Rumbl.Video do
     field(:title, :string)
     field(:description, :string)
     belongs_to(:user, Rumbl.User)
+    belongs_to(:category, Rumbl.Category)
 
     timestamps
   end
@@ -16,5 +17,6 @@ defmodule Rumbl.Video do
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
+    |> assoc_constraint(:category)
   end
 end
