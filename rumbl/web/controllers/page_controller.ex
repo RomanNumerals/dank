c593135c65a27bb277fd9c266d6dcbe6824cbbfb
Rafael Roman <31549567+RomanNumerals@users.noreply.github.com>
2018-04-18 15:40:52 -0400

    deleted more spaces and added a missing module

--- rumbl/web/controllers/page_controller.ex
+++ rumbl/web/controllers/page_controller.ex
@@ -4,4 +4,8 @@ defmodule Rumbl.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  defmodule Rumbl.User do
+    defstruct [:id, :name, :username, :password]
+  end
 end
