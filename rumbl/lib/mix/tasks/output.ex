cdf3fa21f66565a8d323775551e089635b7b6966
Rafael Roman <31549567+RomanNumerals@users.noreply.github.com>
2018-04-20 14:55:55 -0400

    Now my tasks actually look nice! They also output into a table format, gracias ChadOh

--- rumbl/lib/mix/tasks/output.ex
+++ rumbl/lib/mix/tasks/output.ex
@@ -5,18 +5,30 @@ defmodule Mix.Tasks.Output do
   alias Rumbl.Video
 
   @shortdoc "Counts number of videos by each category, and returns them"
-  # solution comprehension/result thanks to Choadis & Colelabar
+  # partial solution comprehension/result thanks to Colelabar & Caleb
 
   def run(_) do
     Mix.Task.run("app.start")
 
-    Repo.all(
-      from(
-        v in Video,
-        join: c in assoc(v, :category),
-        select: count(v.id)
+    list =
+      Repo.all(
+        from(
+          v in Video,
+          join: c in assoc(v, :category),
+          select: {c.name, count(v.id)},
+          group_by: c.name
+        )
       )
-    )
-    |> IO.inspect()
+
+    IO.puts("___________________________ ")
+    IO.puts("| Category | Upload Total |")
+    IO.puts("|__________|______________|")
+
+    Enum.each(list, fn {name, count} ->
+      IO.puts("| #{String.pad_trailing(name, 8)} |       #{count}      |")
+    end)
+
+    IO.puts("|__________|______________|")
+    IO.puts("Hey check out the table, users are adding videos!")
   end
 end
